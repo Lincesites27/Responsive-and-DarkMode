59e5542abd9fdc9f1ef1f649c4b47ffde4d6faad
Lincesites27 <contato@lincesites.com.br>
2022-11-30 22:37:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+trocaCor = () =>{
+    const dark = document.querySelector("#dark")
+
+    dark.classList.toggle("dark")
+}
+
+trocaToogle =()=>{
+    const cor = document.querySelector("#nav-toogle")
+    cor.classList.toggle("open")
+}
